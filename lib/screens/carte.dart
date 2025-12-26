64c9aa8a6d43c921bf3d22a8bbd44369ac6e9471
Lohan Hébert <lohanhebert7@gmail.com>
2025-12-26 09:01:15 +0100

    feat: flutter_map sur carte screen

--- lib/screens/carte.dart
+++ lib/screens/carte.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
 class CartePage extends StatelessWidget {
   const CartePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Carte',
-        style: TextStyle(
-          fontSize: 24,
-          fontWeight: FontWeight.w600,
-        ),
+    return Scaffold(
+      body: Stack(
+        children: [
+          FlutterMap(
+            options: const MapOptions(
+              initialCenter: LatLng(45.566669, 5.93333),
+              initialZoom: 10,
+              maxZoom: 18,
+            ),
+            children: [
+              TileLayer(
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                userAgentPackageName: 'com.example.plantoune',
+              ),
+              RichAttributionWidget(
+                attributions: [
+                  TextSourceAttribution('© OpenStreetMap contributors'),
+                ],
+              ),
+            ],
+          )
+
+        ],
       ),
     );
   }
